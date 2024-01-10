7cbc04f50b06a3c1b715dbd5e247151710026805
TalhaSariii <117735845+TalhaSariii@users.noreply.github.com>
2024-01-11 01:05:00 +0300

    .

--- Blog.Data/Repositories/Abstraction/IRepository.cs
+++ Blog.Data/Repositories/Abstraction/IRepository.cs
@@ -6,6 +6,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Blog.Data.Repositories.Abstraction
 {
     public interface IRepository<T> where T : class,IEntityBase,new()
